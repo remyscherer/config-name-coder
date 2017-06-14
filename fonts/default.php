b32a89caffa66fe00a16e8b94c1a1810352df0be
Remy Scherer <remy.scherer95@gmail.com>
2017-06-14 12:09:12 +0200

    Add files via upload

---
+++ fonts/default.php
@@ -0,0 +1,19 @@
+<?php 
+$font = array(
+    "spalten" => 5,
+    "a" => array(
+        "&nbsp;&nbsp;&nbsp;&nbsp;AA&nbsp;&nbsp;&nbsp;&nbsp;",
+        "&nbsp;&nbsp;&nbsp;A&nbsp;&nbsp;A&nbsp;&nbsp;&nbsp;",
+        "&nbsp;&nbsp;AAAAAA&nbsp;&nbsp;",
+        "&nbsp;A&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A&nbsp;",
+        "A&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A"
+        ),
+    "b" => array(
+        "BBBBBBBB&nbsp;&nbsp;",
+        "B&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BB&nbsp;",
+        "BBBBBBBB&nbsp;&nbsp;",
+        "B&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BB&nbsp;",
+        "BBBBBBBB&nbsp;&nbsp;"
+        )
+    );
+?>
--- index.php
+++ index.php
@@ -6,36 +6,16 @@
 }</style>
 </head>
 <?php
-$default = array(
-    "spalten" => 5,
-    "a" => array(
-        "&nbsp;&nbsp;&nbsp;&nbsp;AA&nbsp;&nbsp;&nbsp;&nbsp;",
-        "&nbsp;&nbsp;&nbsp;A&nbsp;&nbsp;A&nbsp;&nbsp;&nbsp;",
-        "&nbsp;&nbsp;AAAAAA&nbsp;&nbsp;",
-        "&nbsp;A&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A&nbsp;",
-        "A&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A"),
-    "b" => array(
-        "BBBBBBBB&nbsp;&nbsp;",
-        "B&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BB&nbsp;",
-        "BBBBBBBB&nbsp;&nbsp;",
-        "B&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BB&nbsp;",
-        "BBBBBBBB&nbsp;&nbsp;")
-    );
-/*
-1234567890
-BBBBBBBB
-B      BB
-BBBBBBBB
-B      BB
-BBBBBBBB
-*/
+
+@include("fonts/default.php");
+
 $string = "baba";
 $string = str_split($string);
 
 echo "/*<br /><br />";
-for($i=0;$i<$default["spalten"];$i++){
+for($i=0;$i<$font["spalten"];$i++){
     foreach($string as $letter){
-        echo $default[$letter][$i]."&nbsp;";
+        echo $font[$letter][$i]."&nbsp;";
     }
     echo "<br />"; 
 }
